9bcbfe94b9a63376f17643b831f189677f7adeba
marcossilvestrini <marcos.silvestrini@gmail.com>
2025-04-07 23:26:13 -0300

    upd: add kubernetes cluster

---
+++ vagrant/scripts/docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+<<'MULTILINE-COMMENT'
+    Requirments: none
+    Description: Script for set Docker Environment
+    Author: Marcos Silvestrini
+    Date: 07/04/2025
+MULTILINE-COMMENT
+
+# Set language/locale and encoding
+export LANG=C
+
+# Workdir
+cd /home/vagrant || exit
+
+# Install dependencies
+sudo apt install -y \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg \
+lsb-release
+
+# Install docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker vagrant
+sudo systemctl enable docker
+sudo systemctl start docker
+newgrp docker
--- vagrant/scripts/kubernetes.sh
+++ vagrant/scripts/kubernetes.sh
@@ -21,16 +21,6 @@ curl \
 gnupg \
 lsb-release
 
-# Install docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker vagrant
-sudo systemctl enable docker
-sudo systemctl start docker
-newgrp docker
-
 # Install kubectl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -51,3 +41,10 @@ chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
 # criar cluster
+CLUSTER_NAME=ha-cluster
+kind delete cluster --name $CLUSTER_NAME
+kind create cluster  --config configs/kubernetes/kind/cluster.yaml
+
+# exportar kubeconfig
+mkdir -p configs/kubernetes/kubeconfigs
+kind get kubeconfig --name $CLUSTER_NAME > configs/kubernetes/kubeconfigs/$CLUSTER_NAME
